8dc09107198a1475bdd424134389fd9a30d09ca2
yarilaureano <yari.22.naxhiely@gmail.com>
2022-08-06 20:00:05 -0500

    card-attributes: added attributes to class

--- classes/card.py
+++ classes/card.py
@@ -1,2 +1,15 @@
+rank = range(1, 14)
+suit = ('clubs', 'diamonds', 'hearts', 'spades')
+
+
 class Card:
-    pass
+
+    m_rank = None
+    m_suite = None
+    m_is_face_up = False
+
+    def get_value(self):
+        return None
+
+    def flip(self):
+        self.m_is_face_up  = not self.m_is_face_up  
