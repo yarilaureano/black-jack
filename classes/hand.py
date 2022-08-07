51ff34e30b8ab49e19667dcea3ab86802b78878c
yarilaureano <yari.22.naxhiely@gmail.com>
2022-08-06 20:30:09 -0500

    hand-attributes: added atrributes to hand

--- classes/hand.py
+++ classes/hand.py
@@ -1,2 +1,11 @@
 class Hand:
-    pass
+    m_cards = []
+
+    def add(self, card):
+        self.m_cards.append(card)
+
+    def clear(self):
+        self.m_cards = []
+
+    def get_total(self):
+        return None         
